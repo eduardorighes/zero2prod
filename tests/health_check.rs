e008e60dc5c0f9ca34bc7bda327c3b9de605e48d
Eduardo Righes <eduardo.righes@gmail.com>
2025-12-26 21:35:32 +0000

    Added missing explanation.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -28,7 +28,10 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-// Launch the application in the background, somehow...
+// No .await call, therefore no need for `spawn_app` to be async now.
+// We are also running tests, so it is not worth it to propagate errors:
+// if we fail to perform the required setup we can just panic and crash
+// all the things.
 fn spawn_app() -> String {
     let listener = TcpListener::bind("0.0.0.0:0").expect("failed to bind random port");
     let port = listener.local_addr().unwrap().port();
