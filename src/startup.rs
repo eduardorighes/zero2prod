547a3808aac36d981099e27e4e887c8f11c9219a
Eduardo Righes <eduardo.righes@gmail.com>
2026-01-10 15:43:34 +0000

    Use TracingLogger.

--- src/startup.rs
+++ src/startup.rs
@@ -4,6 +4,7 @@ use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer, web};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 // Notice the different signature!
 // We return `Server` on the happy path, and we dropped the `async` keyword
@@ -12,7 +13,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
